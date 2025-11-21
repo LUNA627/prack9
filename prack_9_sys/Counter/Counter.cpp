079a6650893383bf27ed0a5f537511a17d903434
LUNA627 <say.samurai@yandex.ru>
2025-11-22 00:45:36 +0500

    Add files via upload

---
+++ prack_9_sys/Counter/Counter.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <cstdio>
+#include <locale>
+using namespace std;
+
+
+int wmain(int argc, wchar_t* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+
+    void* ptr = nullptr;
+
+    swscanf_s(argv[1], L"%p", &ptr);
+
+    HANDLE hThread = (HANDLE)ptr;
+
+
+
+    int choice;
+
+    do {
+        wcout << "1 — приостановить\n";
+        wcout << "2 — возобновить\n";
+        wcout << "3 — выйти\n";
+        wcin >> choice;
+
+        switch (choice) {
+            case 1: {
+                SuspendThread(hThread);
+                break;
+            }
+
+            case 2: {
+                ResumeThread(hThread);
+                break;
+            }
+
+            case 3: {
+                return 0;
+            }
+        }
+    }
+    while (choice != 3);
+
+
+    return 0;
+}
